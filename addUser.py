72808f5ffe7a96d5a4788c23cdc571c8f946267d
Geno Erickson <geno@Genos-MacBook-Air.local>
2019-09-12 22:04:21 -0500

    python script to add user with public key for login

---
+++ addUser.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import shutil
+
+#assumes arg1 is the username and arg2 is the name of the public key filename
+
+username = sys.argv[1]
+pubkeyFilename = sys.argv[2]
+userAddString = "useradd -m -d /home/" + username+ "  -s /bin/bash " + username
+userSshDir = " /home/"+username+"/.ssh/
+
+os.system(userAddString)
+if os.path.exists(userSshDir + "/authorized_keys"):
+    with open(pubkeyFilename) as f:
+        with open(userSshDir + "/authorized_keys", "w") as f1:
+            for line in f:
+             f1.write(line)
+else:
+     os.system("mkdir " + userSshDir)
+     shutil.copy(pubkeyFilename,userSshDir + "/authorized_keys")
+
+os.system("chown -R " + username + ":" + username + userSshDir)
+os.system("chmod 700 "+ userSshDir)
+os.system("chmod 600 "+ userSshDir + "/authorized_keys")
